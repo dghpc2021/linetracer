2af1f9cad6384a2c66b7a1d4232542be6ba4d914
eunwoo1104 <sions04@naver.com>
2021-09-29 15:52:21 +0900

    Implemented ir sensor

--- motor.py
+++ motor.py
@@ -23,12 +23,14 @@ class Motor:
         GPIO.setup(self.IN4, GPIO.OUT, initial=GPIO.LOW)
         GPIO.setup(self.ENB, GPIO.OUT, initial=GPIO.LOW)
 
-        self.enap = GPIO.PWM(ENA, 200)  # noqa
-        self.enbp = GPIO.PWM(ENB, 200)  # noqa
+        self.enap = GPIO.PWM(self.ENA, 200)  # noqa
+        self.enbp = GPIO.PWM(self.ENB, 200)  # noqa
         self.enap.start(0)
         self.enbp.start(0)
 
         self.__speed = 100
+        self.__speed_left = 100
+        self.__speed_right = 100
 
     def close(self):
         GPIO.output(self.IN1, GPIO.LOW)  # noqa
@@ -48,19 +50,26 @@ class Motor:
         GPIO.output(self.IN3, GPIO.LOW)  # noqa
         GPIO.output(self.IN4, GPIO.HIGH)  # noqa
 
-    def left(self):
+    def right(self):
         GPIO.output(self.IN1, GPIO.LOW)  # noqa
         GPIO.output(self.IN2, GPIO.HIGH)  # noqa
         GPIO.output(self.IN3, GPIO.LOW)  # noqa
         GPIO.output(self.IN4, GPIO.HIGH)  # noqa
 
-    def right(self):
+    def left(self):
         GPIO.output(self.IN1, GPIO.HIGH)  # noqa
         GPIO.output(self.IN2, GPIO.LOW)  # noqa
         GPIO.output(self.IN3, GPIO.HIGH)  # noqa
         GPIO.output(self.IN4, GPIO.LOW)  # noqa
 
     def set_speed(self, speed: int):
-        self.__speed = speed
-        self.enap.ChangeDutyCycle(self.__speed)
-        self.enbp.ChangeDutyCycle(self.__speed)
+        self.__speed_left = speed
+        self.__speed_right = speed
+        self.enap.ChangeDutyCycle(self.__speed_left)
+        self.enbp.ChangeDutyCycle(self.__speed_right)
+
+    def set_individual_speed(self, left_speed: int = None, right_speed: int = None):
+        self.__speed_left = left_speed
+        self.__speed_right = right_speed
+        self.enap.ChangeDutyCycle(self.__speed_left)
+        self.enbp.ChangeDutyCycle(self.__speed_right)
--- tracer.py
+++ tracer.py
@@ -9,18 +9,32 @@ GPIO.setmode(GPIO.BCM)  # noqa
 
 motor = Motor()
 
+IR_LEFT = 9
+IR_MIDDLE = 11
+IR_RIGHT = 10
+
+ir_left = GPIO.setup(IR_LEFT, GPIO.IN)
+ir_middle = GPIO.setup(IR_MIDDLE, GPIO.IN)
+ir_right = GPIO.setup(IR_RIGHT, GPIO.IN)
+
 try:
     motor.forward()
     while True:
+        direction = input("dir: ")
+        if direction.startswith("f"):
+            motor.forward()
+        elif direction.startswith("b"):
+            motor.backward()
+        elif direction.startswith("l"):
+            motor.left()
+        elif direction.startswith("r"):
+            motor.right()
         ipt = int(input("speed: "))
         motor.set_speed(ipt)
+        print(GPIO.input(IR_LEFT), GPIO.input(IR_MIDDLE), GPIO.input(IR_RIGHT))
         time.sleep(5)
 except KeyboardInterrupt:
     pass
 finally:
-        
-    GPIO.output(IN1, GPIO.LOW)  # noqa
-    GPIO.output(IN2, GPIO.LOW)  # noqa
-    GPIO.output(IN3, GPIO.LOW)  # noqa
-    GPIO.output(IN4, GPIO.LOW)  # noqa
+    motor.close()
     GPIO.cleanup()
